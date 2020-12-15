8231965df1f8a7943e41bf102b4d84b6ccf64ba5
yushengyu <yushengyu@58.com>
2020-12-16 00:38:53 +0800

    feat:
    合并两个有序链表

--- lettcode/Array/21.js
+++ lettcode/Array/21.js
@@ -41,7 +41,7 @@ console.log("sum", a);
 var mySqrt = function (x) {
   let left = 0;
   let right = x - 1;
-  
+
   while (left <= right) {
     let mid = (left + (right - left)) >> 1;
     if (mid * mid <= x) {
@@ -52,4 +52,34 @@ var mySqrt = function (x) {
   }
   // return
   return left;
+};
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (l1, l2) {
+  if (l1 == null) {
+    return l2;
+  }
+
+  if (l2 == null) {
+    return l1;
+  }
+
+  if (l1.val <= l2.val) {
+    l1.next = mergeTwoLists(l1.next, l2);
+    return l1;
+  } else if (l1.val > l2.val) {
+    l2.next = mergeTwoLists(l1, l2.next);
+    return l2;
+  }
 };
