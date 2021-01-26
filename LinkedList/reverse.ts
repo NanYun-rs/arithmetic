209c2174e7ec9a91f482e6dc6aba060ad30a9085
yushengyu <yushengyu@58.com>
2021-01-26 23:56:12 +0800

    feat: 添加 ts-node 调试

---
+++ LinkedList/reverse.ts
@@ -0,0 +1,14 @@
+/*
+ * @Author: yushengyu@58.com
+ * @Date: 2021-01-26 23:50:40
+ * @Last Modified by: yushengyu@58.com
+ * @Last Modified time: 2021-01-26 23:53:19
+ */
+
+// @ts-nocheck
+
+// 反转整个链表
+
+const reverse = () => {
+    
+};
